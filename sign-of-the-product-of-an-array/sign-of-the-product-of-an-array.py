21750eed4d04e4e46df2dbcff24f31bfb9e0215d
Mr. Error <122160056+101rror@users.noreply.github.com>
2023-05-21 22:14:26 +0600

    Time: 74 ms (58.76%) | Memory: 16.4 MB (43.27%) - LeetSync

---
+++ sign-of-the-product-of-an-array/sign-of-the-product-of-an-array.py
@@ -0,0 +1,13 @@
+class Solution:
+    def arraySign(self, nums: List[int]) -> int:
+        product = 1
+
+        for i in (nums):
+            product *= i
+
+        if product == 0:
+            return 0
+        elif product > 0:
+            return 1
+        else:
+            return -1
