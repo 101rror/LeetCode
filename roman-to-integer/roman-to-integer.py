0f1425176a0e6cbe40d646c114bb449bc52b272e
Mr. Error <122160056+101rror@users.noreply.github.com>
2023-05-25 11:15:44 +0600

    Time: 62 ms (39.78%) | Memory: 16.3 MB (46.52%) - LeetSync

---
+++ roman-to-integer/roman-to-integer.py
@@ -0,0 +1,21 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
+        ans = 0
+
+        s = s.replace("IV", "IIII").replace("IX", "VIIII")
+        s = s.replace("XL", "XXXX").replace("XC", "LXXXX")
+        s = s.replace("CD", "CCCC").replace("CM", "DCCCC")
+
+        for i in s:
+            ans += roman[i]
+
+        return ans
