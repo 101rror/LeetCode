1c7d2d943cd51b21b57ca0e3014629d1f2b5e62c
Mr. Error <122160056+101rror@users.noreply.github.com>
2023-06-17 15:36:25 +0600

    Time: 857 ms (52.31%) | Memory: 138.9 MB (43.08%) - LeetSync

---
+++ make-array-strictly-increasing/make-array-strictly-increasing.py
@@ -0,0 +1,21 @@
+class Solution:
+    def makeArrayIncreasing(self, arr1: List[int], arr2: List[int]) -> int:
+        arr2 = sorted(arr2)
+
+        @cache
+        def dfs(i, x):
+            if (i == len(arr1)):
+                return 0
+            j = bisect_left(arr2, x+1)
+            return min(
+                dfs(i+1, arr2[j]) + 1
+                    if (j < len(arr2)) else 2001,
+                dfs(i+1, arr1[i])
+                    if (arr1[i] > x) else 2001
+            )
+
+        res = dfs(0, -1)
+        if (res != 2001):
+            return res
+
+        return -1
