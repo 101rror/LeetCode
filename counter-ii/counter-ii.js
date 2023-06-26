11ba92be99600a9db6082b93e35cfe05c9094c39
Mr. Error <122160056+101rror@users.noreply.github.com>
2023-06-26 22:13:41 +0600

    Time: 77 ms (13.15%) | Memory: 44.2 MB (89.92%) - LeetSync

---
+++ counter-ii/counter-ii.js
@@ -0,0 +1,17 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init)
+{
+    let Counter = init
+
+    return{ increment:()=> ++Counter, decrement:()=> --Counter, reset:()=> Counter = init }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
