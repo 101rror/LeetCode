6738cf2eb8b83d5c58174c9e235f52aaa7c56f92
Mr. Error <122160056+101rror@users.noreply.github.com>
2023-06-26 19:00:04 +0600

    Time: 993 ms (26.04%) | Memory: 31 MB (18.93%) - LeetSync

---
+++ total-cost-to-hire-k-workers/total-cost-to-hire-k-workers.py
@@ -0,0 +1,33 @@
+class Solution:
+    def totalCost(self, costs: List[int], k: int, candidates: int) -> int:
+        n = len(costs)
+        heap = []
+
+        i = 0
+        j = n-1
+        x = candidates
+        
+        while(i <= j and x > 0):
+            heappush(heap, (costs[i], i))
+            if (i != j):
+                heappush(heap, (costs[j], j))
+            i += 1
+            j -= 1
+            x -= 1
+    
+        total = 0
+        for _ in range(k):
+            c, ind = heappop(heap)
+            total += c
+        
+            if (i <= j):
+                if (ind < i):
+                    heappush(heap, (costs[i], i))
+                    i += 1
+                else:
+                    heappush(heap, (costs[j], j))
+                    j -= 1
+
+        return total
+            
+                        
