8b87abfc55286b45355d3063f2fdeb28c54f7c4c
Mr. Error <122160056+101rror@users.noreply.github.com>
2023-08-06 22:12:33 +0600

    Time: 51 ms (100.00%) | Memory: 16.3 MB (100.00%) - LeetSync

---
+++ 2886-faulty-keyboard/faulty-keyboard.py
@@ -0,0 +1,11 @@
+class Solution:
+    def finalString(self, s: str) -> str:
+        ans = ""
+
+        for c in s:
+            if(c == "i"):
+                ans = ans[::-1]
+            else:
+                ans += c
+                
+        return ans
