52ff495ecbbcc8f2159a8a4fb6a3f7f7c1307499
Mr. Error <122160056+101rror@users.noreply.github.com>
2023-08-18 20:03:03 +0600

    Time: 52 ms (79.56%) | Memory: 44.2 MB (24.62%) - LeetSync

---
+++ 2805-array-wrapper/array-wrapper.js
@@ -0,0 +1,14 @@
+let ArrayWrapper = function(nums)
+{
+    this.nums = nums;
+};
+
+ArrayWrapper.prototype.valueOf = function()
+{
+    return this.nums.reduce((sum, num) => sum + num, 0);
+}
+
+ArrayWrapper.prototype.toString = function()
+{
+    return `[${this.nums.join(',')}]`;
+}
