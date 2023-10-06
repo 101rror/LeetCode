9057474b58e3c9ea40ee90c169f463bab4148ded
Mr. Error <122160056+101rror@users.noreply.github.com>
2023-10-06 14:38:48 +0600

    Time: 37 ms (75.93%) | Memory: 16.3 MB (58.98%) - LeetSync

---
+++ 343-integer-break/integer-break.py
@@ -0,0 +1,25 @@
+class Solution:
+    def integerBreak(self, n: int) -> int:
+        ans = []
+
+        if(n == 2 or n == 3):
+            return (n - 1)
+        if(n == 4):
+            return n
+        if(n == 5):
+            return (n + 1)
+
+        while(n):
+            if(not n % 3):
+                n -= 3
+                ans.append(3)
+            else:
+                n -= 2
+                ans.append(2)
+
+        res = 1
+        
+        for num in ans:
+            res *= num
+
+        return res
