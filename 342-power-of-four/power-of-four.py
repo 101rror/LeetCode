b9821367f2573ae8d40eed825b7db1ffad333379
Mr. Error <122160056+101rror@users.noreply.github.com>
2023-10-23 14:49:25 +0600

    Time: 31 ms (95.10%) | Memory: 16.1 MB (93.59%) - LeetSync

---
+++ 342-power-of-four/power-of-four.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        if n == 1:
+            return True
+        elif n <= 0 or n % 4 != 0:
+            return False
+        else:
+            t = math.log(n) / math.log(4)
+            if(t == int(t)):
+                return True
