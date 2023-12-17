8886ba03b06855f8da55a1af15bb38e6d0aed1ad
Mr. Error <122160056+101rror@users.noreply.github.com>
2023-12-17 10:42:42 +0600

    Time: 937 ms (50.00%) | Memory: 17 MB (100.00%) - LeetSync

---
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.py
@@ -0,0 +1,27 @@
+class Solution:
+    def findMissingAndRepeatedValues(self, grid: List[List[int]]) -> List[int]:
+        ans = []
+        res = []
+
+        for i in grid:
+            for j in i:
+                ans.append(j)
+
+        ast = list(set(ans))
+        x = 0
+
+        for i in ast:
+            if ans.count(i) > 1:
+                res.append(i)
+        for i in range(1, len(ast)):
+            if abs(ast[i - 1] - ast[i]) != 1:
+                x = ast[i] - 1
+
+        if(x == 0 and ast[0] == 1):
+            res.append(ast[len(ast) - 1] + 1)
+        elif(x == 0 and ast[0] != 1):
+            res.append(ast[0] - 1)
+        else:
+            res.append(x)
+
+        return res
