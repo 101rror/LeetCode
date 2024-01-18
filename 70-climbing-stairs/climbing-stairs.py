85c35e1e67f7608ab10d79f88c9c47ffe390b804
Mr. Error <122160056+101rror@users.noreply.github.com>
2024-01-18 22:57:58 +0600

    Time: 39 ms (43.82%) | Memory: 17.3 MB (31.72%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.py
@@ -0,0 +1,14 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if(n <= 3):
+            return n
+        else:
+            dp = [0] * (n + 1)
+            dp[1]= 1
+            dp[2] = 2
+
+            for i in range(3, n + 1):
+                dp[i] = dp[i - 1] + dp[i - 2]
+
+            print(dp)
+            return dp[n]
