6fa129013e3b18f0e37dbe1297131759afe63e7d
Mr. Error <122160056+101rror@users.noreply.github.com>
2024-02-09 22:58:12 +0600

    Time: 226 ms (53.13%) | Memory: 16.9 MB (76.96%) - LeetSync

---
+++ 368-largest-divisible-subset/largest-divisible-subset.py
@@ -0,0 +1,16 @@
+class Solution:
+    def largestDivisibleSubset(self, nums):
+        n = len(nums)
+        nums.sort()
+        
+        if n == 0:
+            return []
+        
+        dp = [[i] for i in nums]
+         
+        for i in range(n):
+            for j in range(i):
+                if nums[i] % nums[j] == 0 and len(dp[j]) + 1 > len(dp[i]):
+                    dp[i] = dp[j] + [nums[i]]
+        
+        return max(dp, key=len)
