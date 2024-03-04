ce0e3a6be35b4b7479ec4dd6871f28de7494c367
Mr. Error <122160056+101rror@users.noreply.github.com>
2024-03-04 23:03:46 +0600

    Time: 49 ms (81.30%) | Memory: 16.7 MB (64.63%) - LeetSync

---
+++ 985-bag-of-tokens/bag-of-tokens.py
@@ -0,0 +1,28 @@
+class Solution:
+    def bagOfTokensScore(self, token: List[int], power: int) -> int:
+        n = len(token)
+        token.sort()
+        first, last, count, mx = 0, n - 1, 0, 0
+
+        if n == 0:
+            return 0
+        if n == 1:
+            if token[0] <= power:
+                return 1
+            else:
+                return 0
+        
+        while(first <= last):
+            if token[first] <= power:
+                count += 1
+                power -= token[first]
+                first += 1
+                mx = max(mx, count)
+            elif count > 0:
+                count -= 1
+                power += token[last]
+                last -= 1
+            else:
+                break
+
+        return mx
