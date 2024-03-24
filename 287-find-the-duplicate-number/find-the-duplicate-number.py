be09a566d69031a178a2afe3d9981377bff5c461
Mr. Error <122160056+101rror@users.noreply.github.com>
2024-03-24 23:21:28 +0600

    Time: 453 ms (81.57%) | Memory: 32.6 MB (24.76%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/find-the-duplicate-number.py
@@ -1,8 +1,8 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        seen = set()
-        
+        st = set()
+
         for num in nums:
-            if num in seen:
+            if num in st:
                 return num
-            seen.add(num)
+            st.add(num)
