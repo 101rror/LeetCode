668c8d90e4b0a16fff8e8297f1405037fd898f24
Mr. Error <122160056+101rror@users.noreply.github.com>
2024-03-31 12:31:19 +0600

    Time: 726 ms (31.13%) | Memory: 31 MB (80.17%) - LeetSync

---
+++ 2527-count-subarrays-with-fixed-bounds/count-subarrays-with-fixed-bounds.py
@@ -0,0 +1,18 @@
+class Solution:
+    def countSubarrays(self, nums: List[int], minK: int, maxK: int) -> int:
+        n = len(nums)
+        count = 0
+
+        mn = mx = bound = -1
+
+        for i in range(n):
+            if (nums[i] == minK):
+                mn = i
+            if(nums[i] == maxK):
+                mx = i
+            if (not minK <= nums[i] <= maxK):
+                bound = i
+
+            count += max(0, min(mn, mx) - bound)
+        
+        return count
