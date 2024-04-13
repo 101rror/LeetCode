0a830f3a2c6476fd786f9ce67be2aca80255919c
Mr. Error <122160056+101rror@users.noreply.github.com>
2024-04-13 12:07:50 +0600

    Time: 417 ms (29.21%) | Memory: 16.6 MB (44.47%) - LeetSync

---
+++ 3252-count-the-number-of-incremovable-subarrays-i/count-the-number-of-incremovable-subarrays-i.py
@@ -0,0 +1,17 @@
+class Solution:
+    def incremovableSubarrayCount(self, nums: List[int]) -> int:
+        count = 0
+
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums) + 1):
+                l = nums[: i] + nums[j :]
+                if l == sorted(l) and len(l) == len(set(l)):
+                    count += 1
+                    
+        return count
+                        
+                        
+                    
+                
+                
+        
