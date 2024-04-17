a7c39c4ad2053e815e5ae6360a7d6dbad8888dfb
Mr. Error <122160056+101rror@users.noreply.github.com>
2024-04-17 12:44:29 +0600

    Time: 37 ms (54.44%) | Memory: 16.4 MB (95.50%) - LeetSync

---
+++ 3361-latest-time-you-can-obtain-after-replacing-characters/latest-time-you-can-obtain-after-replacing-characters.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findLatestTime(self, s: str) -> str:
+        s = list(s)
+
+        if s[0] == '?': 
+            if s[1] == '?' or s[1] <= '1':
+                s[0] = '1'
+            else:
+                s[0] = '0'
+        if s[1] == '?': 
+            if s[0] == '1':
+                s[1] = '1'
+            else:
+                s[1] = '9'
+        if s[3] == '?':
+            s[3] = '5'
+        if s[4] == '?':
+            s[4] = '9'
+            
+        return ''.join(s)
