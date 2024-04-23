14e4c887aadc973bb2c98ffaae80ca256811d7aa
Mr. Error <122160056+101rror@users.noreply.github.com>
2024-04-23 16:39:38 +0600

    Time: 81 ms (42.42%) | Memory: 22.5 MB (75.45%) - LeetSync

---
+++ 386-lexicographical-numbers/lexicographical-numbers.py
@@ -0,0 +1,20 @@
+class Solution:
+    def lexicalOrder(self, n: int) -> List[int]:
+        ans = []
+        cur = 1
+
+        for i in range(n):
+            ans.append(cur)
+
+            if cur * 10 <= n:
+                cur *= 10
+            else:
+                if cur >= n:
+                    cur //= 10
+
+                cur += 1
+
+                while cur % 10 == 0:
+                    cur //= 10
+                    
+        return ans
