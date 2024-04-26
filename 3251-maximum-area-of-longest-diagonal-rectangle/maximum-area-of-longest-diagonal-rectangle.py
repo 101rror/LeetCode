241737e7936da874dbbe9bcb1ce2fec1f8802076
Mr. Error <122160056+101rror@users.noreply.github.com>
2024-04-26 12:08:39 +0600

    Time: 70 ms (84.60%) | Memory: 16.4 MB (96.28%) - LeetSync

---
+++ 3251-maximum-area-of-longest-diagonal-rectangle/maximum-area-of-longest-diagonal-rectangle.py
@@ -0,0 +1,19 @@
+class Solution:
+    def areaOfMaxDiagonal(self, d: List[List[int]]) -> int:
+        n = len(d)
+        mx = 0
+        m = 0
+        
+        for i in range(n):
+            x, y = d[i]
+            
+            t = x * x + y * y
+            
+            if (t > m) or (t == m and x * y > mx):
+                mx = x * y
+                m = t
+                
+        return mx
+            
+            
+            
