5bd3c8c30cc871e7a7b07f3f1f0b85906c850b4e
Mr. Error <122160056+101rror@users.noreply.github.com>
2024-05-20 20:37:50 +0600

    Time: 93 ms (14.24%) | Memory: 16.6 MB (48.58%) - LeetSync

---
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.py
@@ -0,0 +1,14 @@
+class Solution:
+    def subsetXORSum(self, nums: List[int]) -> int:
+        ans = 0
+        n = len(nums)
+
+        for i in range(1 << n):
+            XOR = 0
+            for j in range(n):
+                if i & 1:
+                    XOR ^= nums[j]
+                i >>= 1
+            ans += XOR
+
+        return ans
