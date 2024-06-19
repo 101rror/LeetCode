506a0533d2c588cca8df8ca1b0cd0e75ce9b7f90
Mr. Error <122160056+101rror@users.noreply.github.com>
2024-06-19 18:43:27 +0600

    Time: 63 ms (15.87%) | Memory: 16.5 MB (52.43%) - LeetSync

---
+++ 3421-count-pairs-that-form-a-complete-day-i/count-pairs-that-form-a-complete-day-i.py
@@ -0,0 +1,10 @@
+class Solution:
+    def countCompleteDayPairs(self, hours: List[int]) -> int:
+        count = 0
+
+        for i in range(0, len(hours) - 1):
+            for j in range(i + 1, len(hours)):
+                if (hours[i] + hours[j]) % 24 == 0:
+                    count += 1
+
+        return count
