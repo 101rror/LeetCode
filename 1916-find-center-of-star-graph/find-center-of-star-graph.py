f40ebc801499db3a29a1564ca2f61273d00a392c
Mr. Error <122160056+101rror@users.noreply.github.com>
2024-06-27 21:37:19 +0600

    Time: 660 ms (55.75%) | Memory: 52.1 MB (86.58%) - LeetSync

--- 1916-find-center-of-star-graph/find-center-of-star-graph.py
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.py
@@ -1,6 +1,6 @@
 class Solution:
     def findCenter(self, e: List[List[int]]) -> int:
-	    if e[0][0]==e[1][0] or e[0][0]==e[1][1]:
+	    if e[0][0] == e[1][0] or e[0][0] == e[1][1]:
 		    return e[0][0]
-            
+
 	    return e[0][1]
