a0a9271e8aeb7f78b3e934321b05fc88f6b16532
Mr. Error <122160056+101rror@users.noreply.github.com>
2024-07-11 21:42:18 +0600

    Time: 31 ms (86.37%) | Memory: 16.6 MB (40.89%) - LeetSync

---
+++ 1298-reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.py
@@ -0,0 +1,16 @@
+class Solution:
+    def reverseParentheses(self, s: str) -> str:
+        stack = []
+        
+        for char in s:
+            if char == ')':
+                temp = []
+                while stack and stack[-1] != '(':
+                    temp.append(stack.pop())
+                stack.pop()
+                stack.extend(temp)
+            else:
+                stack.append(char)
+        
+        return ''.join(stack)
+        
