4ca22afb3dc9803ed442419af4ef2254a0172b8c
Mr. Error <122160056+101rror@users.noreply.github.com>
2024-07-29 22:52:06 +0600

    Time: 493 ms (55.38%) | Memory: 16.6 MB (66.67%) - LeetSync

---
+++ 1511-count-number-of-teams/count-number-of-teams.py
@@ -0,0 +1,24 @@
+class Solution:
+    def numTeams(self, rating: List[int]) -> int:
+        n = len(rating)
+        count = 0
+        
+        for j in range(n):
+            leftLess = leftGreater = 0
+            rightLess = rightGreater = 0
+
+            for i in range(j + 1):
+                if rating[i] < rating[j]:
+                    leftLess += 1
+                elif rating[i] > rating[j]:
+                    leftGreater += 1
+            
+            for k in range(j + 1, n):
+                if rating[k] < rating[j]:
+                    rightLess += 1
+                elif rating[k] > rating[j]:
+                    rightGreater += 1
+            
+            count += leftLess * rightGreater + leftGreater * rightLess
+        
+        return count
