1055c5df9445098fe49b84c601029dbca306c874
Mr. Error <122160056+101rror@users.noreply.github.com>
2024-08-01 13:57:37 +0600

    Time: 60 ms (43.20%) | Memory: 50.5 MB (81.38%) - LeetSync

--- 2727-number-of-senior-citizens/number-of-senior-citizens.js
+++ 2727-number-of-senior-citizens/number-of-senior-citizens.js
@@ -2,13 +2,13 @@
  * @param {string[]} details
  * @return {number}
  */
-var countSeniors = function(details) {
+var countSeniors = function (details) {
     let count = 0;
 
-    for(let i = 0; i < details.length; i++){
+    for (let i = 0; i < details.length; i++) {
         let x = parseInt(details[i].substring(11, 13), 10);
 
-        if (x > 60){
+        if (x > 60) {
             count += 1;
         }
     }
