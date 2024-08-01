13da441a548e4432794b4390dd565cee6f18418a
Mr. Error <122160056+101rror@users.noreply.github.com>
2024-08-01 13:57:30 +0600

    Time: 49 ms (90.45%) | Memory: 50.7 MB (64.92%) - LeetSync

---
+++ 2727-number-of-senior-citizens/number-of-senior-citizens.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string[]} details
+ * @return {number}
+ */
+var countSeniors = function(details) {
+    let count = 0;
+
+    for(let i = 0; i < details.length; i++){
+        let x = parseInt(details[i].substring(11, 13), 10);
+
+        if (x > 60){
+            count += 1;
+        }
+    }
+
+    return count;
+};
