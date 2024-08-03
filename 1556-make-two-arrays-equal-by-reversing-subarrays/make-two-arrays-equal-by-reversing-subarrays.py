1d6f087d5339dafcc30b38ad735d4d9a2393f209
Mr. Error <122160056+101rror@users.noreply.github.com>
2024-08-03 14:29:22 +0600

    Time: 73 ms (64.47%) | Memory: 16.8 MB (59.96%) - LeetSync

--- 1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.py
+++ 1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.py
@@ -1,9 +1,11 @@
 class Solution:
     def canBeEqual(self, target: List[int], arr: List[int]) -> bool:
-        target = sorted(target)
-        arr = sorted(arr)
+        sumtarget = 0
+        sumarr = 0
 
-        if(target == arr):
-            return True
-        return False
-        
+        for i in target:
+            sumtarget += (i * i)
+        for i in arr:
+            sumarr += (i * i)
+
+        return True if sumtarget == sumarr and sum(arr) == sum(target) else False 
