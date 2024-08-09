f442b45b113a44cb73089305653610eed5dcf28c
Mr. Error <122160056+101rror@users.noreply.github.com>
2024-08-09 21:48:29 +0600

    Time: 37 ms (81.03%) | Memory: 16.6 MB (53.33%) - LeetSync

---
+++ 870-magic-squares-in-grid/magic-squares-in-grid.py
@@ -0,0 +1,28 @@
+class Solution:
+    def numMagicSquaresInside(self, grid: List[List[int]]) -> int:
+        n = len(grid)
+        m = len(grid[0])
+        count = 0
+
+        square = [
+            [[4, 9, 2], [3, 5, 7], [8, 1, 6]],
+            [[2, 7, 6], [9, 5, 1], [4, 3, 8]],
+            [[6, 1, 8], [7, 5, 3], [2, 9, 4]],
+            [[8, 3, 4], [1, 5, 9], [6, 7, 2]],
+            [[4, 3, 8], [9, 5, 1], [2, 7, 6]],
+            [[2, 9, 4], [7, 5, 3], [6, 1, 8]],
+            [[6, 7, 2], [1, 5, 9], [8, 3, 4]],
+            [[8, 1, 6], [3, 5, 7], [4, 9, 2]]
+        ]
+
+        if n < 3 or m < 3:
+            return 0
+        
+        for row in range(n - 2):
+            for col in range(m - 2):
+                subgrid = [grid[row + i][col : col + 3] for i in range(3)]
+
+                if subgrid in square:
+                    count += 1
+
+        return count
