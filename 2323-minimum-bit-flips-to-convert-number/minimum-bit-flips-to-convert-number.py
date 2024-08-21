bbb3323afef40e67beaf85857558e319af797f48
Mr. Error <122160056+101rror@users.noreply.github.com>
2024-08-21 23:09:29 +0600

    Time: 38 ms (38.22%) | Memory: 16.4 MB (66.41%) - LeetSync

---
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minBitFlips(self, s: int, g: int) -> int:
+        count = 0 
+
+        while s or g:
+            if s % 2 != g % 2:
+                count += 1
+
+            s, g = s // 2, g // 2
+            
+        return count
