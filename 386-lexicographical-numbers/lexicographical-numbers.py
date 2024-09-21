ccc42f5f369b646e3e65d1c7fc44b2a02fd028ca
Error <122160056+101rror@users.noreply.github.com>
2024-09-21 13:10:31 +0600

    Time: 60 ms (96.17%) | Memory: 22.3 MB (91.75%) - LeetSync

--- 386-lexicographical-numbers/lexicographical-numbers.py
+++ 386-lexicographical-numbers/lexicographical-numbers.py
@@ -8,13 +8,14 @@ class Solution:
 
             if cur * 10 <= n:
                 cur *= 10
+
             else:
                 if cur >= n:
                     cur //= 10
 
                 cur += 1
 
-                while cur % 10 == 0:
+                while(cur % 10 == 0):
                     cur //= 10
-                    
+
         return ans
