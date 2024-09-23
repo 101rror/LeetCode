5ae514c2c0f70ebde5cdecc75f95a198032042fa
Error <122160056+101rror@users.noreply.github.com>
2024-09-23 20:38:17 +0600

    Time: 204 ms (53.42%) | Memory: 16.7 MB (63.57%) - LeetSync

---
+++ 2755-extra-characters-in-a-string/extra-characters-in-a-string.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minExtraChar(self, s: str, dictionary: List[str]) -> int:
+        n = len(s)
+        dp = [n] * (n + 1)
+
+        for i in range(1, n + 1):
+            for word in dictionary:
+                if i >= len(word) and s[i - len(word) : i] == word:
+                    dp[i] = min(dp[i], dp[i-len(word)]-len(word))
+
+            dp[i] = min(dp[i], dp[i - 1])
+
+        return dp[n]
