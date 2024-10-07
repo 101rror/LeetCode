8225b694fa8406b88f3f29033396d4df65d334ba
Error <122160056+101rror@users.noreply.github.com>
2024-10-07 12:08:26 +0600

    Time: 45 ms (51.93%) | Memory: 16.6 MB (7.81%) - LeetSync

---
+++ 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.py
@@ -0,0 +1,17 @@
+class Solution:
+    def minLength(self, s: str) -> int:
+        stack = []
+
+        for char in s:
+            if not stack:
+                stack.append(char)
+                continue
+            if char == "B" and stack[-1] == "A":
+                stack.pop()
+            elif char == "D" and stack[-1] == "C":
+                stack.pop()
+            else:
+                stack.append(char)
+
+
+        return len(stack)
