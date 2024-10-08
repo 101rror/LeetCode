607bbc97df880036762552ba970a39233e6c7883
Error <122160056+101rror@users.noreply.github.com>
2024-10-08 13:23:20 +0600

    Time: 175 ms (85.96%) | Memory: 29.3 MB (19.60%) - LeetSync

---
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minSwaps(self, s: str) -> int:
+        stack = []
+        count = 0
+
+        for b in s:
+            if b == '[':
+                stack.append(b)
+            else:
+                if stack:
+                    stack.pop()
+                else:
+                    count += 1
+
+        return (count + 1) // 2
