03126dd0c5851736ff9a612e74aaa4191ccea1e7
Error <122160056+101rror@users.noreply.github.com>
2024-10-30 19:41:56 +0600

    Time: 1869 ms (41.86%) | Memory: 16.8 MB (62.07%) - LeetSync

---
+++ 1766-minimum-number-of-removals-to-make-mountain-array/minimum-number-of-removals-to-make-mountain-array.py
@@ -0,0 +1,22 @@
+class Solution:
+    def minimumMountainRemovals(self, nums: List[int]) -> int:
+        n = len(nums)
+        maxmount = 0
+        increase = [1] * n
+        decrease = [1] * n
+
+        for i in range(n):
+            for j in range(i):
+                if nums[j] < nums[i]:
+                    increase[i] = max(increase[i], increase[j] + 1)
+        
+        for i in range(n-1, -1, -1):
+            for j in range(i+1, n):
+                if nums[j] < nums[i]:
+                    decrease[i] = max(decrease[i], decrease[j] + 1)
+
+        for i in range(n):
+            if increase[i] > 1 and decrease[i] > 1:
+                maxmount = max(maxmount, increase[i] + decrease[i] - 1)
+
+        return n - maxmount
