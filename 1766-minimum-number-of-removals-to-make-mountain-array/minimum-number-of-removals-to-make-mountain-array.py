b1c2ca7ce249233ee9828cbecd929ab6545123ca
Error <122160056+101rror@users.noreply.github.com>
2024-10-30 19:42:11 +0600

    Time: 1860 ms (46.51%) | Memory: 16.8 MB (62.07%) - LeetSync

--- 1766-minimum-number-of-removals-to-make-mountain-array/minimum-number-of-removals-to-make-mountain-array.py
+++ 1766-minimum-number-of-removals-to-make-mountain-array/minimum-number-of-removals-to-make-mountain-array.py
@@ -9,9 +9,9 @@ class Solution:
             for j in range(i):
                 if nums[j] < nums[i]:
                     increase[i] = max(increase[i], increase[j] + 1)
-        
-        for i in range(n-1, -1, -1):
-            for j in range(i+1, n):
+
+        for i in range(n - 1, -1, -1):
+            for j in range(i + 1, n):
                 if nums[j] < nums[i]:
                     decrease[i] = max(decrease[i], decrease[j] + 1)
 
