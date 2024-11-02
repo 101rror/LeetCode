9227272b03af49153cca9a1d2a7b8fbf7ed8157a
Error <122160056+101rror@users.noreply.github.com>
2024-11-02 13:56:59 +0600

    Time: 3 ms (12.78%) | Memory: 16.6 MB (11.84%) - LeetSync

--- 2580-circular-sentence/circular-sentence.py
+++ 2580-circular-sentence/circular-sentence.py
@@ -1,23 +1,7 @@
 class Solution:
     def isCircularSentence(self, sentence: str) -> bool:
-        lst = list(sentence.split())
+        for i in range(len(sentence)):
+            if sentence[i] == ' ' and sentence[i - 1] != sentence[i + 1]:
+                return False
 
-        if len(lst) == 1:
-            return sentence[0] == sentence[len(sentence) - 1]
-
-        if sentence[0] == sentence[len(sentence) - 1]:
-            ans = []
-
-            for i in range(len(lst)):
-                s = lst[i]
-                ans.append(s[0])
-                if len(s) > 1:
-                    ans.append(s[len(s) - 1])
-
-            for i in range(1, len(ans) - 1):
-                if ans[i - 1] != ans[i] != ans[i + 1]:
-                    return False
-
-            return True
-
-        return False
+        return sentence[0] == sentence[len(sentence) - 1]
