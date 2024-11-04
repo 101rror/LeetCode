8811f5b83c8e8af45e73cb3bf7d220a6af80ff30
Error <122160056+101rror@users.noreply.github.com>
2024-11-04 10:53:36 +0600

    Time: 263 ms (44.68%) | Memory: 27.4 MB (16.78%) - LeetSync

--- 3451-string-compression-iii/string-compression-iii.py
+++ 3451-string-compression-iii/string-compression-iii.py
@@ -1,46 +1,25 @@
 class Solution:
     def compressedString(self, word: str) -> str:
-        ans = ""
-        count = 1
-
         if len(word) == 1:
-            ans += str(count)
-            ans += word
+            return "1" + word
+
+        ans = []
+        count = 1
 
         for i in range(1, len(word)):
             if word[i - 1] == word[i]:
                 count += 1
             else:
-                if count <= 9:
-                    ans += str(count)
-                    ans += word[i - 1]
-                    count = 1
-                else:
-                    while count:
-                        if count >= 9:
-                            ans += str(9)
-                            ans += word[i - 1]
-                            count -= 9
-                        else:
-                            ans += str(count)
-                            ans += word[i - 1]
-                            count -= count
-
-                    count = 1
+                while count > 9:
+                    ans.append("9" + word[i - 1])
+                    count -= 9
+                ans.append(str(count) + word[i - 1])
+                count = 1
 
             if i == len(word) - 1:
-                if count > 9:
-                    while count:
-                        if count >= 9:
-                            ans += str(9)
-                            ans += word[i]
-                            count -= 9
-                        else:
-                            ans += str(count)
-                            ans += word[i]
-                            count -= count
-                else:
-                    ans += str(count)
-                    ans += word[i]
+                while count > 9:
+                    ans.append("9" + word[i])
+                    count -= 9
+                ans.append(str(count) + word[i])
 
-        return ans
+        return "".join(ans)
