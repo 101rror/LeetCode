c324f5d1ce8f8d41f1e783d2037a5c3f7582413d
Error <122160056+101rror@users.noreply.github.com>
2024-11-30 21:09:18 +0600

    Time: 296 ms (86.88%) | Memory: 93.7 MB (32.90%) - LeetSync

---
+++ 2201-valid-arrangement-of-pairs/valid-arrangement-of-pairs.py
@@ -0,0 +1,26 @@
+class Solution:
+    def validArrangement(self, pairs: List[List[int]]) -> List[List[int]]:
+        graph = defaultdict(list)
+        degree = defaultdict(int)
+
+        for x, y in pairs: 
+            graph[x].append(y)
+            degree[x] += 1
+            degree[y] -= 1
+                
+        for k in degree: 
+            if degree[k] == 1: 
+                x = k
+                break 
+                
+        ans = []
+
+        def fn(x): 
+            while graph[x]:
+                fn(graph[x].pop()) 
+            ans.append(x)
+        
+        fn(x)
+        ans.reverse()
+
+        return [[ans[i], ans[i+1]] for i in range(len(ans) - 1)]
