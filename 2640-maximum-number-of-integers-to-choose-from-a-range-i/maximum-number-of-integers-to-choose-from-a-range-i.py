7b0ef5cd6a4c81bc446ed8c087f7356b5582e0d3
Error <122160056+101rror@users.noreply.github.com>
2024-12-06 21:11:41 +0600

    Time: 83 ms (20.61%) | Memory: 18.6 MB (28.75%) - LeetSync

--- 2640-maximum-number-of-integers-to-choose-from-a-range-i/maximum-number-of-integers-to-choose-from-a-range-i.py
+++ 2640-maximum-number-of-integers-to-choose-from-a-range-i/maximum-number-of-integers-to-choose-from-a-range-i.py
@@ -1,11 +1,11 @@
 class Solution:
     def maxCount(self, banned: List[int], n: int, maxSum: int) -> int:
-        st = set(banned)
+        counter = Counter(banned)
         cursum = 0
         count = 0
 
         for i in range(1, n + 1):
-            if i not in st:
+            if i not in counter:
                 cursum += i
                 if cursum <= maxSum:
                     count += 1
