6dae22aaaa43dc749f5bf5d63c717fc0d6212d26
Error <122160056+101rror@users.noreply.github.com>
2024-12-08 11:05:28 +0600

    Time: 51 ms (100.00%) | Memory: 17.5 MB (100.00%) - LeetSync

---
+++ 3621-minimum-operations-to-make-array-values-equal-to-k/minimum-operations-to-make-array-values-equal-to-k.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minOperations(self, nums: List[int], k: int) -> int:
+        st = list(set(nums))
+        count = 0
+
+        for num in st:
+            if num < k:
+                return -1
+            if num > k:
+                count += 1
+
+        return count
