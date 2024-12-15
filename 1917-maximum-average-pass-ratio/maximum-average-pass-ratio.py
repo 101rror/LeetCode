890688c1972951f8cb1dc1e0e889b1092607786c
Error <122160056+101rror@users.noreply.github.com>
2024-12-15 22:57:27 +0600

    Time: 941 ms (89.33%) | Memory: 54.3 MB (37.04%) - LeetSync

---
+++ 1917-maximum-average-pass-ratio/maximum-average-pass-ratio.py
@@ -0,0 +1,22 @@
+class Solution:
+    def maxAverageRatio(self, classes: List[List[int]], k: int) -> float:
+        n = len(classes)
+        tsum = 0
+        heap = []
+
+        for p, q in classes:
+            tsum += p / q
+            heap.append(((p - q) / (q * (q + 1)), p, q))
+
+        heapify(heap)
+
+        for _ in range(k):
+            (r, p, q) = heap[0]
+            if r == 0:
+                break
+            tsum -= r
+            x = (p - q) / ((q + 1.0) * (q + 2.0))
+            heapreplace(heap, (x, p + 1, q + 1))
+
+
+        return tsum / n
