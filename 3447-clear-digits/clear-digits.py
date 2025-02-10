afb645d08a3cbbe55e1e06a500ac6ef045695fed
Error <122160056+101rror@users.noreply.github.com>
2025-02-10 21:56:06 +0600

    Time: 3 ms (30.49%) | Memory: 17.8 MB (27.31%) - LeetSync

--- 3447-clear-digits/clear-digits.py
+++ 3447-clear-digits/clear-digits.py
@@ -1,13 +1,11 @@
 class Solution:
     def clearDigits(self, s: str) -> str:
-        st = []
+        stack = []
 
         for ch in s:
-            if '0' <= ch <= '9' and st:
-                st.pop()
+            if "0" <= ch <= "9" and stack:
+                stack.pop()
             else:
-                st.append(ch)
-        
-        s = "".join(st)
-        
-        return s
+                stack.append(ch)
+
+        return "".join(stack)
