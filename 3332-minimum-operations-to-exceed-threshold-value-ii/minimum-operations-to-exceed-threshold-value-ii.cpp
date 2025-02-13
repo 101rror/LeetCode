23607c62fd049e9ebef591d60b85c201749827ee
Error <122160056+101rror@users.noreply.github.com>
2025-02-13 22:44:34 +0600

    Time: 83 ms (93.90%) | Memory: 99.1 MB (39.26%) - LeetSync

---
+++ 3332-minimum-operations-to-exceed-threshold-value-ii/minimum-operations-to-exceed-threshold-value-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+        priority_queue<long long, vector<long long>, greater<long long>> pq;
+
+        for (auto i : nums) {
+            pq.push(i);
+        }
+
+        int ans = 0;
+
+        while (!pq.empty() && pq.top() < k) {
+            long long x = pq.top();
+            pq.pop();
+            long long y = pq.top();
+            pq.pop();
+
+            pq.push(min(x, y) * 2 + max(x, y));
+            ans++;
+        }
+
+        return ans;
+    }
+};
