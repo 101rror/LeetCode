bb6c3394615d21f328cdddcd9379f62610ab6032
Error <122160056+101rror@users.noreply.github.com>
2025-02-14 23:41:41 +0600

    Time: 34 ms (66.21%) | Memory: 31.1 MB (98.63%) - LeetSync

---
+++ 1477-product-of-the-last-k-numbers/product-of-the-last-k-numbers.py
@@ -0,0 +1,27 @@
+class ProductOfNumbers:
+
+    def __init__(self):
+        self.list = []
+        self.prod = 1
+
+    def add(self, num: int) -> None:
+        if num == 0:
+            self.list = []
+            self.prod = 1
+        else:
+            self.prod *= num
+            self.list.append(self.prod)
+            
+
+    def getProduct(self, k: int) -> int:
+        if len(self.list) < k:
+            return 0
+        if len(self.list) == k:
+            return self.list[-1]
+        return self.list[-1] // self.list[-k-1]
+
+
+# Your ProductOfNumbers object will be instantiated and called as such:
+# obj = ProductOfNumbers()
+# obj.add(num)
+# param_2 = obj.getProduct(k)
