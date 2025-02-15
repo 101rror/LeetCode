e5bd44c180309b4c55f4ed3d6a9cc6a531a13ae5
Error <122160056+101rror@users.noreply.github.com>
2025-02-15 22:32:43 +0600

    Time: 0 ms (100.00%) | Memory: 17.7 MB (92.56%) - LeetSync

---
+++ 2802-find-the-punishment-number-of-an-integer/find-the-punishment-number-of-an-integer.py
@@ -0,0 +1,14 @@
+class Solution:
+    def punishmentNumber(self, n: int) -> int:
+        arr = [1,9,10,36,45,55,82,91,99,100,235,297,369,370,379,414,
+               657,675,703,756,792,909,918,945,964,990,991,999,1000]
+        
+        ans = 0
+
+        for num in arr:
+            if num <= n:
+                ans += num * num
+            else:
+                break
+
+        return ans
