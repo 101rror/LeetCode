373906756470c9dc97fb0ef010e10063246b1900
Error <122160056+101rror@users.noreply.github.com>
2025-02-19 22:53:04 +0600

    Time: 2 ms (83.81%) | Memory: 17.7 MB (82.93%) - LeetSync

---
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.py
@@ -0,0 +1,18 @@
+class Solution:
+    def getHappyString(self, n: int, k: int) -> str:
+        n2 = n
+
+        def dfs(prefix, n, k):
+            if n == 0:
+                return prefix
+            for c in "abc":
+                if prefix and c == prefix[-1]:
+                    continue
+                cnt = 2 ** (n2 - len(prefix) - 1)
+                if cnt >= k:
+                    return dfs(prefix + c, n - 1, k)
+                else:
+                    k -= cnt
+            return ""
+
+        return dfs("", n, k)
