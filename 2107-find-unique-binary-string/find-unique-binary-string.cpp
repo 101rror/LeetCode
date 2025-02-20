e28650117bb328e655baecf854af22cfe51a6afd
Error <122160056+101rror@users.noreply.github.com>
2025-02-20 22:11:31 +0600

    Time: 0 ms (100.00%) | Memory: 12.6 MB (84.00%) - LeetSync

---
+++ 2107-find-unique-binary-string/find-unique-binary-string.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string findDifferentBinaryString(vector<string>& nums) {
+        string ans;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i][i] == '0') {
+                ans += '1';
+            } else {
+                ans += '0';
+            }
+        }
+
+        return ans;
+    }
+};
