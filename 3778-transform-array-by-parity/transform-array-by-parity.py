24a3de2394d69fd31a87105831830ee8eeda352f
Error <122160056+101rror@users.noreply.github.com>
2025-03-08 23:05:17 +0600

    Time: 2 ms (36.36%) | Memory: 17.7 MB (98.16%) - LeetSync

---
+++ 3778-transform-array-by-parity/transform-array-by-parity.py
@@ -0,0 +1,12 @@
+class Solution:
+    def transformArray(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        
+        for i in range(n):
+            if nums[i] % 2 == 0:
+                nums[i] = 0
+            else:
+                nums[i] = 1
+
+        nums.sort()
+        return nums
