03ab73941d43f85f208b25daa20f4933cbd72d0d
Error <122160056+101rror@users.noreply.github.com>
2025-03-17 23:40:31 +0600

    Time: 3 ms (58.51%) | Memory: 18.2 MB (31.79%) - LeetSync

---
+++ 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool divideArray(vector<int>& nums) {
+        unordered_map<int, int> counter;
+
+        for (auto num : nums) {
+            counter[num]++;
+        }
+
+        for (const auto & [_, count] : counter) {
+            if (count % 2 != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
