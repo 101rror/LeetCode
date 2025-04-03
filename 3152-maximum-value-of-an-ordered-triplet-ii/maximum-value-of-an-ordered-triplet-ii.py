ff1da4ff5ac3b9f4d95ffc32c1be9e83c63a1faf
Error <122160056+101rror@users.noreply.github.com>
2025-04-04 00:39:10 +0600

    Time: 182 ms (54.27%) | Memory: 29.4 MB (67.92%) - LeetSync

---
+++ 3152-maximum-value-of-an-ordered-triplet-ii/maximum-value-of-an-ordered-triplet-ii.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maximumTripletValue(self, nums: List[int]) -> int:
+        n = len(nums)
+        ans, imax, dmax = 0, 0, 0
+
+        for k in range(n):
+            ans = max(ans, dmax * nums[k])
+            dmax = max(dmax, imax - nums[k])
+            imax = max(imax, nums[k])
+
+        return ans
