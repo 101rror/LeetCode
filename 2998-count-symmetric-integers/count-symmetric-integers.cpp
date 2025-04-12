d176d7554b1e4e3044f184f736ba03f90e595892
Error <122160056+101rror@users.noreply.github.com>
2025-04-12 22:34:07 +0600

    Time: 5 ms (89.27%) | Memory: 9.6 MB (83.91%) - LeetSync

---
+++ 2998-count-symmetric-integers/count-symmetric-integers.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int countSymmetricIntegers(int low, int high) {
+        int ans = 0;
+
+        for (int a = low; a <= high; ++a) {
+            if (a < 100 && a % 11 == 0) {
+                ans++;
+            } else if (1000 <= a && a < 10000) {
+                int left = a / 1000 + (a % 1000) / 100;
+                int right = (a % 100) / 10 + a % 10;
+
+                if (left == right) {
+                    ans++;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
