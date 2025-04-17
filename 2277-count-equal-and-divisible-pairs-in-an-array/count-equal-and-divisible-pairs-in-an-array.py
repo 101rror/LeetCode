bd2478bd69fa2f518c271a01ebe97763bfdf9441
Error <mr.101rror@gmail.com>
2025-04-17 21:46:36 +0600

    Time: 23 ms (49.67%) | Memory: 17.9 MB (48.17%) - LeetSync

--- 2277-count-equal-and-divisible-pairs-in-an-array/count-equal-and-divisible-pairs-in-an-array.py
+++ 2277-count-equal-and-divisible-pairs-in-an-array/count-equal-and-divisible-pairs-in-an-array.py
@@ -5,8 +5,8 @@ class Solution:
 
         for i in range(0, n - 1):
             for j in range(i + 1, n):
-                if(nums[i] == nums[j]):
-                    if((i * j) % k == 0):
+                if nums[i] == nums[j]:
+                    if (i * j) % k == 0:
                         count += 1
 
         return count
