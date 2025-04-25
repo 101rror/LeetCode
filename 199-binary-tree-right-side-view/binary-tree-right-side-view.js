4aebe9c1d3ba3035d8767f72b0fe95a64cec0b6f
Error <mr.101rror@gmail.com>
2025-04-26 00:02:29 +0600

    Time: 0 ms (100.00%) | Memory: 55.4 MB (37.46%) - LeetSync

---
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var rightSideView = function (root) {
+    if (!root) {
+        return [];
+    }
+
+    let res = [];
+    pre(root, 0);
+    return res;
+
+    function pre(node, h) {
+        if (!node) {
+            return;
+        }
+        
+        res[h] = node.val;
+        pre(node.left, h + 1);
+        pre(node.right, h + 1);
+    }
+};
