f86f85702c376b2dc0e2984f37cb7750f01b72a5
Error <mr.101rror@gmail.com>
2025-04-26 21:57:44 +0600

    Time: 174 ms (34.44%) | Memory: 28.9 MB (16.39%) - LeetSync

--- 2527-count-subarrays-with-fixed-bounds/count-subarrays-with-fixed-bounds.py
+++ 2527-count-subarrays-with-fixed-bounds/count-subarrays-with-fixed-bounds.py
@@ -6,13 +6,13 @@ class Solution:
         mn = mx = bound = -1
 
         for i in range(n):
-            if (nums[i] == minK):
+            if nums[i] == minK:
                 mn = i
-            if(nums[i] == maxK):
+            if nums[i] == maxK:
                 mx = i
-            if (not minK <= nums[i] <= maxK):
+            if not minK <= nums[i] <= maxK:
                 bound = i
 
             count += max(0, min(mn, mx) - bound)
-        
+
         return count
