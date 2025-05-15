58ac734ba3513d1eea8d950db70c167fe31fdd91
Error <mr.101rror@gmail.com>
2025-05-15 12:01:43 +0600

    Time: 0 ms (100.00%) | Memory: 18 MB (5.36%) - LeetSync

--- 3143-longest-unequal-adjacent-groups-subsequence-i/longest-unequal-adjacent-groups-subsequence-i.py
+++ 3143-longest-unequal-adjacent-groups-subsequence-i/longest-unequal-adjacent-groups-subsequence-i.py
@@ -1,13 +1,11 @@
 class Solution:
-    def getWordsInLongestSubsequence(self, n: int, words: List[str], groups: List[int]) -> List[str]:
+    def getLongestSubsequence(self, words: List[str], groups: List[int]) -> List[str]:
         n = len(groups)
         ans = []
         ans.append(words[0])
-        
+
         for i in range(0, n - 1):
-            if(groups[i] != groups[i + 1]):
+            if groups[i] != groups[i + 1]:
                 ans.append(words[i + 1])
-                
-                
+
         return ans
-        
