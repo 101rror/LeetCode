79cf42383de5364c4d00925b08991304ce5a1b04
Error <mr.101rror@gmail.com>
2025-07-21 23:05:30 +0600

    Time: 305 ms (73.16%) | Memory: 19.6 MB (66.40%) - LeetSync

--- 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.py
+++ 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.py
@@ -1,12 +1,12 @@
 class Solution:
     def makeFancyString(self, s: str) -> str:
+        n = len(s)
         ans = s[0]
         count = 1
 
-        for i in range(1, len(s)):
+        for i in range(1, n):
             if s[i] == ans[-1]:
                 count += 1
-
                 if count < 3:
                     ans += s[i]
             else:
