1dc9a695fdb9eb31bc35f4abf96c2e5fe604ea21
Error <mr.101rror@gmail.com>
2025-08-28 13:02:22 +0600

    Time: 48 ms (82.46%) | Memory: 54.7 MB (64.30%) - LeetSync

--- 2741-function-composition/function-composition.js
+++ 2741-function-composition/function-composition.js
@@ -2,8 +2,19 @@
  * @param {Function[]} functions
  * @return {Function}
  */
-var compose = function(functions) {
-    return x => functions.reduceRight((acc,f)=>f(acc),x)
+var compose = function (functions) {
+    return function (x) {
+        if (functions.length === 0) {
+            return x;
+        }
+
+        var ans = functions[functions.length - 1](x);
+
+        for (var i = functions.length - 2; i >= 0; i--) {
+            ans = functions[i](ans);
+        }
+        return ans;
+    };
 };
 
 /**
