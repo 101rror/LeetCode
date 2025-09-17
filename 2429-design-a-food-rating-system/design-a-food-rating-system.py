99f9269440147cf9c156da013166a65631616495
Error <mr.101rror@gmail.com>
2025-09-17 21:34:53 +0600

    Time: 218 ms (34.56%) | Memory: 50.2 MB (96.31%) - LeetSync

--- 2429-design-a-food-rating-system/design-a-food-rating-system.py
+++ 2429-design-a-food-rating-system/design-a-food-rating-system.py
@@ -1,11 +1,12 @@
 from sortedcontainers import SortedList
 
+
 class FoodRatings:
     def __init__(self, foods: List[str], cuisines: List[str], ratings: List[int]):
         self.mp = {}
         self.data = defaultdict(SortedList)
 
-        for food, cuisine, rating in zip(foods, cuisines, ratings): 
+        for food, cuisine, rating in zip(foods, cuisines, ratings):
             self.mp[food] = (cuisine, rating)
             self.data[cuisine].add((-rating, food))
 
