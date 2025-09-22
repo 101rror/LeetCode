1d63f9bf1f9bc1c998ee8f5d188451e1f94b7c6f
Error <mr.101rror@gmail.com>
2025-09-22 11:41:41 +0600

    Time: 3 ms (34.62%) | Memory: 17.7 MB (86.68%) - LeetSync

--- 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.py
+++ 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.py
@@ -2,22 +2,22 @@ class Solution:
     def maxFrequencyElements(self, nums: List[int]) -> int:
         mp = defaultdict(int)
         ans = []
-        
-        for i in nums:
-            mp[i] += 1
-            
-        for i in mp.values():
-            ans.append(i)
-            
-        ans = sorted(ans, reverse = True)
-        
-        if(ans[0] == 1):
+
+        for num in nums:
+            mp[num] += 1
+
+        for val in mp.values():
+            ans.append(val)
+
+        ans = sorted(ans, reverse=True)
+
+        if ans[0] == 1:
             return sum(ans)
         else:
             mx = ans[0]
             tsum = 0
-            for i in ans:
-                if(i == mx):
-                    tsum += i
-                    
+            for num in ans:
+                if num == mx:
+                    tsum += num
+
             return tsum
