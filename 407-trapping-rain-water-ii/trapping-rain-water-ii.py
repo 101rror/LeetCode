c1b6212a0e8c511f218805f0a7ebac3b4a06220d
Error <mr.101rror@gmail.com>
2025-10-03 20:54:53 +0600

    Time: 119 ms (84.75%) | Memory: 20.2 MB (72.63%) - LeetSync

---
+++ 407-trapping-rain-water-ii/trapping-rain-water-ii.py
@@ -0,0 +1,36 @@
+import heapq
+
+class Solution:
+    def trapRainWater(self, heightMap: List[List[int]]) -> int:
+        m, n = len(heightMap), len(heightMap[0])
+
+        if not heightMap or not heightMap[0] or m < 3 or n < 3:
+            return 0
+
+        heap = []
+        visited = [[False] * n for _ in range(m)]
+
+        for i in range(m):
+            for j in range(n):
+                if i == 0 or i == m - 1 or j == 0 or j == n - 1:
+                    heapq.heappush(heap, (heightMap[i][j], i, j))
+                    visited[i][j] = True
+
+        res = 0
+        directions = [(1,0), (-1,0), (0,1), (0,-1)]
+
+        while heap:
+            height, x, y = heapq.heappop(heap)
+
+            for dx, dy in directions:
+                nx, ny = x + dx, y + dy
+                if 0 <= nx < m and 0 <= ny < n and not visited[nx][ny]:
+                    visited[nx][ny] = True
+                    nh = heightMap[nx][ny]
+
+                    if nh < height:
+                        res += height - nh
+
+                    heapq.heappush(heap, (max(nh, height), nx, ny))
+
+        return res
