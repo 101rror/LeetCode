ccaa9b0f3d2a772089b69db2f03b3042e11b1981
Error <mr.101rror@gmail.com>
2025-10-15 21:43:37 +0600

    Time: 0 ms (100.00%) | Memory: 63 MB (48.13%) - LeetSync

---
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int n = height.size();
+        int left = 0, right = n - 1;
+        int ans = 0;
+
+        while (left <= right) {
+            int currentArea = min(height[left], height[right]) * (right - left);
+            ans = max(ans, currentArea);
+
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return ans;
+    }
+};
