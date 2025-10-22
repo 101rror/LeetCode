1edd9fe032aefb5e8bc9c905dac096b88ada2de2
Error <mr.101rror@gmail.com>
2025-10-22 21:31:43 +0600

    Time: 536 ms (79.03%) | Memory: 37.3 MB (85.48%) - LeetSync

--- 3640-maximum-frequency-of-an-element-after-performing-operations-ii/maximum-frequency-of-an-element-after-performing-operations-ii.py
+++ 3640-maximum-frequency-of-an-element-after-performing-operations-ii/maximum-frequency-of-an-element-after-performing-operations-ii.py
@@ -2,10 +2,6 @@ class Solution:
     def maxFrequency(self, nums: List[int], k: int, numOperations: int) -> int:
         nums.sort()
         n = len(nums)
-
-        if not nums:
-            return 0
-
         freq = Counter(nums)
         ans = 1
 
@@ -14,16 +10,14 @@ class Solution:
             high = v + k
             L = bisect.bisect_left(nums, low)
             R = bisect.bisect_right(nums, high)
-            mid = R - L
-            need = mid - t
-            x = min(need, numOperations)
+            mid = R - L - t
+            x = min(mid, numOperations)
             ans = max(ans, t + x)
 
         l = 0
         for r in range(n):
             while l <= r and nums[r] - nums[l] > 2 * k:
                 l += 1
-            w = r - l + 1
-            ans = max(ans, min(w, numOperations))
+            ans = max(ans, min(r - l + 1, numOperations))
 
         return ans
