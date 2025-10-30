d0d7c90eac98ca4aa3ff9fce130656b5d925289f
Error <mr.101rror@gmail.com>
2025-10-30 09:53:51 +0600

    Time: 4 ms (9.01%) | Memory: 17.9 MB (40.36%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.py
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.py
@@ -1,23 +1,14 @@
 class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
-        def isunique(lst):
-            for i in range(1, len(lst)):
-                if(lst[i - 1] == lst[i]):
-                    return False
-            return True
-
         mp = defaultdict(int)
-        ans = []
+        st = set()
 
-        for i in arr:
-            mp[i] += 1
+        for num in arr:
+            mp[num] += 1
 
-        for i in mp.values():
-            ans.append(i)
+        st = set(mp.values())
 
-        ans.sort()
+        x = len(mp.values())
+        y = len(st)
 
-        if(isunique(ans)):
-            return True
-        else:
-            return False
+        return x == y
