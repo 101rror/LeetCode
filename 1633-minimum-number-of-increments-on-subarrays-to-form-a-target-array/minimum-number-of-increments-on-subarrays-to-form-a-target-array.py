5714d4a452c8f5070e60ba1fe33b88ce4304725c
Error <mr.101rror@gmail.com>
2025-10-30 09:41:02 +0600

    Time: 14 ms (93.91%) | Memory: 26.7 MB (36.79%) - LeetSync

--- 1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array/minimum-number-of-increments-on-subarrays-to-form-a-target-array.py
+++ 1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array/minimum-number-of-increments-on-subarrays-to-form-a-target-array.py
@@ -1,12 +1,11 @@
 class Solution:
     def minNumberOperations(self, target: List[int]) -> int:
-        prev = 0
-        res = 0
+        prev, ans = 0, 0
 
         for num in target:
             if num > prev:
                 diff = num - prev
-                res += diff
+                ans += diff
             prev = num
 
-        return res
+        return ans
