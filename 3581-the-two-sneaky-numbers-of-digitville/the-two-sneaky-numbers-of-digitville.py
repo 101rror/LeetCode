8c288ae88c5b7cb7250916bcb1bc944465689239
Error <mr.101rror@gmail.com>
2025-10-31 22:03:01 +0600

    Time: 0 ms (100.00%) | Memory: 17.7 MB (66.29%) - LeetSync

--- 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.py
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.py
@@ -1,13 +1,12 @@
 class Solution:
     def getSneakyNumbers(self, nums: List[int]) -> List[int]:
-        mp = defaultdict(int)
+        seen = set()
         ans = []
 
         for num in nums:
-            mp[num] += 1
-
-        for key, val in mp.items():
-            if val == 2:
-                ans.append(key)
+            if num not in seen:
+                seen.add(num)
+            else:
+                ans.append(num)
 
         return ans
