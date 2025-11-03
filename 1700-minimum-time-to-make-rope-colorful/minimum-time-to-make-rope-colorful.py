58d0ee54c2207e1db8aec1f8307ebd09e3144d3a
Error <mr.101rror@gmail.com>
2025-11-03 11:33:46 +0600

    Time: 114 ms (58.76%) | Memory: 26.6 MB (18.81%) - LeetSync

--- 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.py
+++ 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.py
@@ -1,14 +1,14 @@
 class Solution:
-    def minCost(self, co: str, tm: List[int]) -> int:
-        n = len(co)
-        tsum, pre = 0, 0
+    def minCost(self, colors: str, neededTime: List[int]) -> int:
+        n = len(colors)
+        ans, mx = 0, neededTime[0]
 
         for i in range(1, n):
-            if(co[pre] != co[i]):
-                pre = i
+            if colors[i - 1] == colors[i]:
+                mn = min(mx, neededTime[i])
+                ans += mn
+                mx = max(mx, neededTime[i])
             else:
-                tsum += min(tm[pre], tm[i])
-                if(tm[pre] < tm[i]):
-                    pre = i
+                mx = neededTime[i]
 
-        return tsum
+        return ans
