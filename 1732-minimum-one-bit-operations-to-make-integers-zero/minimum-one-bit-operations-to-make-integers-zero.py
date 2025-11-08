6aced03b79b98d3c37f509fb693b332fcb954f83
Error <mr.101rror@gmail.com>
2025-11-08 11:03:03 +0600

    Time: 0 ms (100.00%) | Memory: 17.6 MB (97.83%) - LeetSync

--- 1732-minimum-one-bit-operations-to-make-integers-zero/minimum-one-bit-operations-to-make-integers-zero.py
+++ 1732-minimum-one-bit-operations-to-make-integers-zero/minimum-one-bit-operations-to-make-integers-zero.py
@@ -2,9 +2,9 @@ class Solution:
     def minimumOneBitOperations(self, n: int) -> int:
         count = 0
 
-        while(n):
-            x = (n ^ (n - 1))
-            count = - count - x
-            n &= (n - 1)
+        while n:
+            x = n ^ (n - 1)
+            count = -count - x
+            n &= n - 1
 
         return abs(count)
