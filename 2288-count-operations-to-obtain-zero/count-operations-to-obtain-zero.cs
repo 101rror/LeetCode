1a39263ad60aa0b4d187cc9bd44f66c60e37b8ce
Error <mr.101rror@gmail.com>
2025-11-09 23:53:52 +0600

    Time: 1 ms (82.61%) | Memory: 29.6 MB (23.91%) - LeetSync

---
+++ 2288-count-operations-to-obtain-zero/count-operations-to-obtain-zero.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int CountOperations(int num1, int num2) {
+        int count = 0;
+
+        while (num1 != 0 && num2 != 0) {
+            if (num1 > num2) {
+                num1 -= num2;
+            }
+            else {
+                num2 -= num1;
+            }
+            count++;
+        }
+
+        return count;
+    }
+}
