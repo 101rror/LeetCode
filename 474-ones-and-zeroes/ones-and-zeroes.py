aac1b9daa14d21fc9e1ea8768051171ac84ec185
Error <mr.101rror@gmail.com>
2025-11-11 23:13:42 +0600

    Time: 1566 ms (64.97%) | Memory: 18 MB (83.93%) - LeetSync

---
+++ 474-ones-and-zeroes/ones-and-zeroes.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+
+        for s in strs:
+            zeros, ones = s.count("0"), s.count("1")
+            for i in range(m, zeros - 1, -1):
+                for j in range(n, ones - 1, -1):
+                    dp[i][j] = max(1 + dp[i - zeros][j - ones], dp[i][j])
+
+        return dp[-1][-1]
