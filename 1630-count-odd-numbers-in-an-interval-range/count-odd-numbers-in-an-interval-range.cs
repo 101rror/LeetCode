d70440be87ad57decc44a97e87669ffcd1fe7bfa
Error <mr.101rror@gmail.com>
2025-12-07 08:54:28 +0600

    Time: 21 ms (89.72%) | Memory: 29.3 MB (15.89%) - LeetSync

---
+++ 1630-count-odd-numbers-in-an-interval-range/count-odd-numbers-in-an-interval-range.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int CountOdds(int low, int high) {
+        if (low % 2 == 0) {
+            low++;
+        }
+        if (high % 2 == 0) {
+            high--;
+        }
+
+        return(((high - low) / 2) + 1);
+    }
+}
