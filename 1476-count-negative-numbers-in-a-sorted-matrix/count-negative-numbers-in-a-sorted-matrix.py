5a1a95d4760da530895f0856eca3ef338c467fa9
Error <mr.101rror@gmail.com>
2025-12-28 20:31:40 +0600

    Time: 0 ms (100.00%) | Memory: 18.5 MB (85.71%) - LeetSync

--- 1476-count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.py
+++ 1476-count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.py
@@ -1,10 +1,25 @@
 class Solution:
     def countNegatives(self, grid: List[List[int]]) -> int:
+        def BinarySearch(arr):
+            n = len(arr)
+            count = 0
+
+            first = 0
+            last = n - 1
+
+            while first <= last:
+                mid = (first + last) // 2
+
+                if arr[mid] < 0:
+                    last = mid - 1
+                else:
+                    first = mid + 1
+
+            return first
+
         count = 0
 
-        for i in grid:
-            for j in i[::-1]:
-                if j < 0:
-                    count += 1
+        for lst in grid:
+            count += len(lst) - BinarySearch(lst)
 
         return count
