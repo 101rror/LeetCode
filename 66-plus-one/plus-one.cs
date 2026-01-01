9a557c0089ace08852a501e626a3b631d49d9c2c
Error <mr.101rror@gmail.com>
2026-01-01 20:04:03 +0600

    Time: 0 ms (100.00%) | Memory: 46.5 MB (78.07%) - LeetSync

---
+++ 66-plus-one/plus-one.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public int[] PlusOne(int[] digits) {
+        int n = digits.Length;
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+
+            digits[i] = 0;
+        }
+
+        int[] ans = new int[n + 1];
+        ans[0] = 1;
+
+        return ans;
+    }
+}
