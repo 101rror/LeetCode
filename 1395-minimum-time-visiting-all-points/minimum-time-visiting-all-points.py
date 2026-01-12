635b7f55f50eb9a689dd649cddee55e1b60dd782
Error <mr.101rror@gmail.com>
2026-01-13 00:21:41 +0600

    Time: 0 ms (100.00%) | Memory: 19.5 MB (6.71%) - LeetSync

--- 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.py
+++ 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.py
@@ -1,8 +1,11 @@
-class Solution(object):
+class Solution:
     def minTimeToVisitAllPoints(self, points):
         ans = 0
+        n = len(points)
 
-        for i in range(1, len(points)):
-            ans += max(abs(points[i][0] - points[i-1][0]), abs(points[i][1] - points[i-1][1]))
+        for i in range(1, n):
+            difx = abs(points[i][0] - points[i - 1][0])
+            dify = abs(points[i][1] - points[i - 1][1])
+            ans += max(difx, dify)
 
         return ans
