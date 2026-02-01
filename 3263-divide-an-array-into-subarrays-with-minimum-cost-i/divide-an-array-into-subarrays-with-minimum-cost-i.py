995e53a09c07a6cac7614fd606bbe23701acaddd
Error <mr.101rror@gmail.com>
2026-02-01 12:15:06 +0600

    Time: 0 ms (100.00%) | Memory: 19.3 MB (48.99%) - LeetSync

--- 3263-divide-an-array-into-subarrays-with-minimum-cost-i/divide-an-array-into-subarrays-with-minimum-cost-i.py
+++ 3263-divide-an-array-into-subarrays-with-minimum-cost-i/divide-an-array-into-subarrays-with-minimum-cost-i.py
@@ -1,12 +1,8 @@
 class Solution:
     def minimumCost(self, nums: List[int]) -> int:
-        if len(nums) == 3:
-            return sum(nums)
-        else:
-            tsum = nums[0]
-            nums.pop(0)
-            nums.sort()
-            tsum += nums[0] + nums[1]
-            
-            return tsum
-            
+        first = nums[0]
+        nums.pop(0)
+        nums.sort()
+        first += nums[0] + nums[1]
+
+        return first
