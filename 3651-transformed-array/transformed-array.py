e0f1cb6e162244a1fea52dc0a1249be8d3449e02
Error <mr.101rror@gmail.com>
2026-02-05 21:30:24 +0600

    Time: 55 ms (79.82%) | Memory: 19.3 MB (56.05%) - LeetSync

--- 3651-transformed-array/transformed-array.py
+++ 3651-transformed-array/transformed-array.py
@@ -4,10 +4,7 @@ class Solution:
         ans = [0] * n
 
         for i in range(n):
-            if nums[i] == 0:
-                ans[i] = 0
-            else:
-                x = (i + nums[i]) % n
-                ans[i] = nums[x]
+            x = (i + nums[i]) % n
+            ans[i] = nums[x]
 
         return ans
