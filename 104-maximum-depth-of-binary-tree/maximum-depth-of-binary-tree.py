43f477fdf55b11cba386126cfe1fb18e13cbc5de
Error <mr.101rror@gmail.com>
2026-02-08 12:33:05 +0600

    Time: 1 ms (37.88%) | Memory: 20.5 MB (15.20%) - LeetSync

---
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        def dfs(root):
+            if not root:
+                return 0
+
+            left = dfs(root.left)
+            right = dfs(root.right)
+
+            return max(left, right) + 1
+
+        return dfs(root)
