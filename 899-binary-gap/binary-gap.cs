8a272db2014936073c770c3b026250c2a5e17d20
Error <mr.101rror@gmail.com>
2026-02-22 14:17:31 +0600

    Time: 0 ms (100.00%) | Memory: 28.4 MB (100.00%) - LeetSync

---
+++ 899-binary-gap/binary-gap.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public int BinaryGap(int n) {
+        int ans = 0, pos = 0, last = -1;
+
+        while (n > 0) {
+            if ((n & 1) == 1) {
+                if (last != -1) {
+                    ans = Math.Max(ans, pos - last);
+                }
+                
+                last = pos;
+            }
+
+            n >>= 1;
+            pos++;
+        }
+
+        return ans;
+    }
+}
