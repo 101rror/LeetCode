2f8d303cf50a0dbd14f3f58d63e6f99c7bfa204c
Error <mr.101rror@gmail.com>
2026-02-28 23:35:47 +0600

    Time: 45 ms (90.91%) | Memory: 29.4 MB (18.18%) - LeetSync

--- 1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.cs
+++ 1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.cs
@@ -1,8 +1,7 @@
 public class Solution {
     public int ConcatenatedBinary(int n) {
         const int MOD = 1000000007;
-        long ans = 0;
-        var pow = 2;
+        long ans = 0, pow = 2;
 
         for (int i = 1; i <= n; i++) {
             if (pow == i) {
