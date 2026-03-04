33e0b5752dd532d36e0d6c86b64f39de02cfb226
Error <mr.101rror@gmail.com>
2026-03-04 23:26:17 +0600

    Time: 2 ms (89.97%) | Memory: 19.7 MB (85.11%) - LeetSync

--- 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.py
+++ 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.py
@@ -1,4 +1,4 @@
-class Solution:    
+class Solution:
     def numSpecial(self, mat: List[List[int]]) -> int:
         def column(matrix, i):
             return [row[i] for row in matrix]
@@ -7,13 +7,10 @@ class Solution:
         n = len(mat)
 
         for i in range(n):
-            if(mat[i].count(1) == 1):
+            if mat[i].count(1) == 1:
                 i = mat[i].index(1)
                 col = column(mat, i)
-                if(col.count(1) == 1):
+                if col.count(1) == 1:
                     count += 1
-        
-        return count  
-                
-        
-                
+
+        return count
