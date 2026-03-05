353c38a208fc28de6064b05438ecf164067d783c
Error <mr.101rror@gmail.com>
2026-03-05 23:13:51 +0600

    Time: 11 ms (41.04%) | Memory: 19.3 MB (83.47%) - LeetSync

--- 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.py
+++ 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.py
@@ -3,25 +3,26 @@ class Solution:
         n = len(s)
         count = 0
         count1 = 0
-        if(s.count('1') == n or s.count('0') == n):
+
+        if s.count("1") == n or s.count("0") == n:
             return n // 2
 
         f = s[0]
 
         for i in range(n):
-            if(i % 2 == 0):
-                if(f != s[i]):
+            if i % 2 == 0:
+                if f != s[i]:
                     count += 1
             else:
-                if(f == s[i]):
+                if f == s[i]:
                     count += 1
 
         for i in range(n):
-            if(i % 2 == 0):
-                if(f == s[i]):
+            if i % 2 == 0:
+                if f == s[i]:
                     count1 += 1
             else:
-                if(f != s[i]):
+                if f != s[i]:
                     count1 += 1
 
         return min(count, count1)
