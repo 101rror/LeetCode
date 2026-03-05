dc90f9c4c9044833d61b70fd48aeeb692adad4a3
Error <mr.101rror@gmail.com>
2026-03-05 23:15:59 +0600

    Time: 6 ms (88.05%) | Memory: 19.3 MB (56.18%) - LeetSync

--- 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.py
+++ 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.py
@@ -1,28 +1,18 @@
 class Solution:
     def minOperations(self, s: str) -> int:
-        n = len(s)
         count = 0
         count1 = 0
 
-        if s.count("1") == n or s.count("0") == n:
-            return n // 2
-
-        f = s[0]
-
-        for i in range(n):
+        for i in range(len(s)):
             if i % 2 == 0:
-                if f != s[i]:
+                if s[i] == "1":
                     count += 1
-            else:
-                if f == s[i]:
-                    count += 1
-
-        for i in range(n):
-            if i % 2 == 0:
-                if f == s[i]:
+                if s[i] == "0":
                     count1 += 1
             else:
-                if f != s[i]:
+                if s[i] == "0":
+                    count += 1
+                if s[i] == "1":
                     count1 += 1
 
         return min(count, count1)
