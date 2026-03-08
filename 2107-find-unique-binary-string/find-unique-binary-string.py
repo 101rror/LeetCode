43fe1bedd1e1d0282a2f5be274ac9a6079c96c89
Error <mr.101rror@gmail.com>
2026-03-08 23:41:34 +0600

    Time: 0 ms (100.00%) | Memory: 19.2 MB (99.21%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.py
+++ 2107-find-unique-binary-string/find-unique-binary-string.py
@@ -1,8 +1,9 @@
 class Solution:
     def findDifferentBinaryString(self, nums: List[str]) -> str:
+        n = len(nums)
         ans = ""
 
-        for i in range(len(nums)):
+        for i in range(n):
             if nums[i][i] == "0":
                 ans += "1"
             else:
