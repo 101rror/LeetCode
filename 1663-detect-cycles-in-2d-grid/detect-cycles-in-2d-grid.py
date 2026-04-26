014780b2ba062b8c70b532fc9c50150b3640995d
Error <mr.101rror@gmail.com>
2026-04-26 22:40:17 +0600

    Time: 231 ms (93.35%) | Memory: 53.7 MB (92.83%) - LeetSync

---
+++ 1663-detect-cycles-in-2d-grid/detect-cycles-in-2d-grid.py
@@ -0,0 +1,31 @@
+class Solution:
+    def containsCycle(self, grid: List[List[str]]) -> bool:
+        m, n = len(grid), len(grid[0])
+        visited = [[False] * n for _ in range(m)]
+
+        dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+        for r in range(m):
+            for c in range(n):
+                if visited[r][c]:
+                    continue
+
+                stack = [(r, c, -1, -1)]
+                visited[r][c] = True
+
+                while stack:
+                    cr, cc, pr, pc = stack.pop()
+
+                    for dr, dc in dirs:
+                        nr, nc = cr + dr, cc + dc
+
+                        if (nr < 0 or nr >= m or nc < 0 or nc >= n) or (grid[nr][nc] != grid[cr][cc]) or (nr == pr and nc == pc):
+                            continue
+
+                        if visited[nr][nc]:
+                            return True
+
+                        visited[nr][nc] = True
+                        stack.append((nr, nc, cr, cc))
+
+        return False
