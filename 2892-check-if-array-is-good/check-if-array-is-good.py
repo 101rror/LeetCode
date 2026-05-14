252019142a9b74347fe47a7fc0c14a87efe72bed
Salman Ahmed <mr.101rror@gmail.com>
2026-05-14 21:25:45 +0600

    Time: 3 ms (29.89%) | Memory: 19.2 MB (89.68%) - LeetSync

--- 2892-check-if-array-is-good/check-if-array-is-good.py
+++ 2892-check-if-array-is-good/check-if-array-is-good.py
@@ -9,4 +9,10 @@ class Solution:
         if slen < n - 1 or mx == n or count != 2 or mx > n:
             return False
         else:
+            new = list(seen)
+            for i in range(1, slen):
+                x = new[i] - new[i - 1]
+                if x != 1:
+                    return False
+
             return True
