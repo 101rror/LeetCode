b2c0f2055e840dcb6edd152377ad847a5ccd27d6
Salman Ahmed <mr.101rror@gmail.com>
2026-05-22 15:42:59 +0600

    Time: 0 ms (100.00%) | Memory: 43.1 MB (6.21%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cs
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cs
@@ -1,10 +1,22 @@
 public class Solution {
     public int Search(int[] nums, int target) {
-        int n = nums.Length;
+        int first = 0, last = nums.Length - 1;
 
-        for (int i = 0; i < n; i++) {
-            if (nums[i] == target) {
-                return i;
+        while (first <= last) {
+            int mid = (first + last) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            bool leftSorted = nums[first] < nums[mid];
+            bool targetLeft = nums[first] <= target && target < nums[mid];
+            bool rotatedCase = nums[first] > nums[mid] && (target < nums[mid] || target >= nums[first]);
+
+            if ((leftSorted && targetLeft) || rotatedCase) {
+                last = mid - 1;
+            } else {
+                first = mid + 1;
             }
         }
 
