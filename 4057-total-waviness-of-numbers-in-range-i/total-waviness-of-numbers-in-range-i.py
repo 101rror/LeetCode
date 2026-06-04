29135a3c9c269fae3191c60550e520bd1352bd13
Salman Ahmed <mr.101rror@gmail.com>
2026-06-04 21:37:02 +0600

    Time: 311 ms (62.07%) | Memory: 47.2 MB (5.36%) - LeetSync

--- 4057-total-waviness-of-numbers-in-range-i/total-waviness-of-numbers-in-range-i.py
+++ 4057-total-waviness-of-numbers-in-range-i/total-waviness-of-numbers-in-range-i.py
@@ -1,18 +1,44 @@
 class Solution:
     def totalWaviness(self, num1: int, num2: int) -> int:
-        def waviness(num):
-            x = str(num)
-            n = len(x)
-            count = 0
 
-            for i in range(1, n - 1):
-                if x[i - 1] < x[i] > x[i + 1] or x[i - 1] > x[i] < x[i + 1]:
-                    count += 1
+        def solve(x: int) -> int:
+            s = str(x)
 
-            return count
+            @cache
+            def dp(pos, tight, started, prev1, prev2):
+                if pos == len(s):
+                    return (1, 0)
 
-        count = 0
-        for num in range(num1, num2 + 1):
-            count += waviness(num)
+                limit = int(s[pos]) if tight else 9
 
-        return count
+                ways = 0
+                total = 0
+
+                for d in range(limit + 1):
+                    ntight = tight and d == limit
+
+                    if not started and d == 0:
+                        cnt, wav = dp(pos + 1, ntight, False, -1, -1)
+                        ways += cnt
+                        total += wav
+                    else:
+                        if not started:
+                            cnt, wav = dp(pos + 1, ntight, True, d, -1)
+                            ways += cnt
+                            total += wav
+                        else:
+                            add = 0
+                            if prev2 != -1:
+                                if (prev2 < prev1 > d) or (prev2 > prev1 < d):
+                                    add = 1
+
+                            cnt, wav = dp(pos + 1, ntight, True, d, prev1)
+
+                            ways += cnt
+                            total += wav + add * cnt
+
+                return ways, total
+
+            return dp(0, True, False, -1, -1)[1]
+
+        return solve(num2) - solve(num1 - 1)
