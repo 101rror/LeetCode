ac2e8eaab2777891954a3e333051e37789b3fc16
Salman Ahmed <mr.101rror@gmail.com>
2026-06-15 13:29:11 +0600

    Time: 92 ms (53.17%) | Memory: 61.6 MB (99.45%) - LeetSync

--- 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.py
+++ 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.py
@@ -5,7 +5,7 @@
 #         self.next = next
 class Solution:
     def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if not head or not head.next:
+        if not head.next:
             return None
 
         dq = deque()
