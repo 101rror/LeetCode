48428cd6d3a58a5cf4860ab056b8a53adc98dc8c
Salman Ahmed <mr.101rror@gmail.com>
2026-07-28 21:44:37 +0600

    Time: 248 ms (59.45%) | Memory: 20.1 MB (99.54%) - LeetSync

--- 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.py
+++ 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.py
@@ -1,18 +1,9 @@
 class Solution:
     def smallestPalindrome(self, s: str) -> str:
         n = len(s)
+        half = "".join(sorted(s[: n // 2]))
 
-        if n % 2 == 0:
-            c = list(s[: int(n / 2)])
-            c.sort()
-            x = "".join(c)
-            return x + x[::-1]
+        if n % 2:
+            return half + s[n // 2] + half[::-1]
 
-        else:
-            m = s[int((n + 1) / 2) - 1]
-            c = list(s[: int((n - 1) / 2)])
-            c.sort()
-            x = "".join(c)
-            return x + m + x[::-1]
-
-        return 0
+        return half + half[::-1]
