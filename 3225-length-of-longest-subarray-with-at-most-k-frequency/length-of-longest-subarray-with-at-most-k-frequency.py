ca73bc4775768c4ae5f4533154d60c845b36646b
Salman Ahmed <mr.101rror@gmail.com>
2026-08-12 11:26:29 +0600

    Time: 243 ms (80.44%) | Memory: 35.3 MB (38.32%) - LeetSync

--- 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.py
+++ 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.py
@@ -3,11 +3,11 @@ class Solution:
         mp = defaultdict(int)
         ans = num = 0
 
-        for i, x in enumerate(nums): 
+        for i, x in enumerate(nums):
             mp[x] += 1
-            while mp[x] > k: 
+            while mp[x] > k:
                 mp[nums[num]] -= 1
                 num += 1
             ans = max(ans, i - num + 1)
 
-        return ans 
+        return ans
